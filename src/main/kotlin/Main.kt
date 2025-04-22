56c7be40bdbfc229d6172575f1cdd484ce689c64
xuhaowen2000 <xuhaowen2000@bytedance.com>
2025-04-22 14:52:37 +0800

    高阶函数示例

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+//高阶函数
+fun main()
+{
+    val input = readln()
+    val output = input.myFilter {
+        //it.isLetter()
+        this.isLetter()
+    }
+    println(output)
+}
+
+fun String.myFilter(predicate : Char.() -> Boolean) : String{
+    return buildString{
+        for (ch in this@myFilter){
+            if(ch.predicate()){
+                this.append(ch)
+            }
+        }
+    }
+}
